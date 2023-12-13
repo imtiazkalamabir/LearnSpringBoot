33902e0061a6c998eb59f47b4fcaa82a31a1823c
Imtiaz Kalam Abir <imtiaz.abir@bjitgroup.com>
2023-12-13 10:49:24 +0600

    Section 5 completed

--- src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -15,7 +15,15 @@ public class CourseController {
     public List<Course> retrieveAllCourses() {
         return Arrays.asList(
                 new Course(1, "Learn AWS", "in28minutes"),
-                new Course(1, "Learn AWS", "in28minutes")
+                new Course(2, "Learn AWS", "in28minutes"),
+                new Course(3, "Learn DataScience", "in28minutes"),
+                new Course(4, "Learn Python", "in28minutes"),
+                new Course(5, "Learn Java", "in28minutes"),
+                new Course(6, "Learn Kotlin", "in28minutes"),
+                new Course(7, "Learn Flutter", "in28minutes"),
+                new Course(8, "Learn Flutter", "in28minutes"),
+                new Course(9, "Learn NodeJS", "in28minutes"),
+                new Course(10, "Learn React Native", "in28minutes")
         );
     }
 }
---
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CurrencyConfigurationController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+// http://localhost:8080/currency-configuration
+// Currency configuration
+@RestController
+public class CurrencyConfigurationController {
+
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+
+    @RequestMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retrieveAllCourses() {
+        return configuration;
+    }
+}
